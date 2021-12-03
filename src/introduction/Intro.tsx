fadba4fedad8630357d7fefc5214541590c894e5
Prince <this.is.kumar.prince@gmail.com>
2021-12-03 10:05:17 +0530

    basic tpescript react-three-fiber scene setup

--- src/introduction/Intro.tsx
+++ src/introduction/Intro.tsx
@@ -1,5 +1,37 @@
+import * as THREE from "three";
+import { Canvas, useFrame } from "@react-three/fiber";
+import { useRef, useState } from "react";
+
+const Box: React.FC<JSX.IntrinsicElements["mesh"]> = (props) => {
+  const mesh = useRef<THREE.Mesh>(null!);
+  const [hovered, setHover] = useState(false);
+  const [active, setActive] = useState(false);
+
+  useFrame((state, delta) => (mesh.current.rotation.x += 0.01));
+  return (
+    <mesh
+      {...props}
+      ref={mesh}
+      scale={active ? 1.5 : 1}
+      onClick={() => setActive((active) => !active)}
+      onPointerOver={() => setHover(true)}
+      onPointerOut={() => setHover(false)}
+    >
+      <boxGeometry args={[1, 1, 1]} />
+      <meshStandardMaterial color={hovered ? "hotpink" : "orange"} />
+    </mesh>
+  );
+};
+
 const Intro = () => {
-  return <div>hi</div>;
+  return (
+    <Canvas>
+      <ambientLight />
+      <pointLight position={[10, 10, 10]} />
+      <Box position={[-1.2, 0, 0]} />
+      <Box position={[1.2, 0, 0]} />
+    </Canvas>
+  );
 };
 
 export default Intro;
